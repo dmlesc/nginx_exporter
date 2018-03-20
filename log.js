ca7e7fc3ec6f6438a3416626021a2151730a90a6
dmlesc <dmlurth@gmail.com>
2018-03-19 22:52:20 -0600

    used tail module instead of spawn tail -f

--- log.js
+++ log.js
@@ -1,7 +1,14 @@
 'use strict';
 
-var log = (message) => {
-  console.log(message);
+var log = (label, message0, message1) => {
+  var mess = new Date().toJSON() + ' - ' + label + ': ' + message0;
+  //var mess = '- ' + label + ': ' + message0;
+
+  if (message1 !== undefined) {
+    mess += ' - ' + message1;
+  }
+
+  console.log(mess);
 }
 
 module.exports = log;
--- nginx_exporter.js
+++ nginx_exporter.js
@@ -1,8 +1,9 @@
 'use strict';
 var log = require('./log');
-process.on('uncaughtException', (err) => { log(err); });
+process.on('uncaughtException', (err) => { log('uncaught', err, err.stack); });
 
-const { spawn } = require('child_process');
+var s_pos = 8;
+var rt_pos = 23;
 
 var m = require('./metrics');
 m();
@@ -11,26 +12,23 @@ m.register('request_time_ms_total');
 m.register('request_count', 'status');
 
 const tail_file = '/var/log/nginx/access.log';
-
-function spawns(file) {
-  const tcpdump = spawn('tail', ['-f', file]);
-
-  tcpdump.stderr.on('data', (data) => { log(`stderr: ${data}`); });
-  tcpdump.on('close', (code) => { log('close:', code); });
+const Tail = require('tail').Tail;
+const tail = new Tail(tail_file);
+ 
+tail.on('line', (data) => {
+  //log(data);
+  var line = data.split(' ');
+  var status = line[s_pos];
+  var response_time = sec2ms(line[rt_pos]);
   
-  tcpdump.stdout.on('data', (data) => {
-    //log(data.toString());
-    var line = data.toString().split(' ');
-    var status = line[8];
-    var response_time = sec2ms(line[line.length - 1]);
-    
-    //log(status + ' - ' + response_time);
-    
-    m.inc('requests_total');
-    m.inc('request_time_ms_total', response_time);
-    m.incLabels('request_count', 'status', status);
-  });
-}
+  log('log', status, response_time);
+  
+  m.inc('requests_total');
+  m.inc('request_time_ms_total', response_time);
+  m.incLabels('request_count', 'status', status);
+});
+ 
+tail.on("error", (err) => { log('ERROR', err, err.stack); });
 
 function sec2ms(str) {
   var str = str.split('.');
@@ -39,5 +37,3 @@ function sec2ms(str) {
   var ms = (seconds * 1000) + decimal;
   return ms;
 }
-
-spawns(tail_file);
